2d5afd101740d57a640a0a8172b0fe734420603e
Diego García <dagarciam1014@gmail.com>
2025-05-11 14:25:29 -0500

    Updated

--- DiegoG.ToDoListBot/ToDoListBotWorker.cs
+++ DiegoG.ToDoListBot/ToDoListBotWorker.cs
@@ -8,6 +8,19 @@ public class ToDoListBotWorker(ILogger<ToDoListBotWorker> logger, ToDoListBot bo
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         Logger.LogInformation("ToDoListBotWorker running at: {time}", DateTimeOffset.Now);
-        await Bot.Run(stoppingToken);
+        while (stoppingToken.IsCancellationRequested is false)
+        {
+            try
+            {
+                Logger.LogInformation("Starting ToDoListBot");
+                await Bot.Run(stoppingToken);
+            }
+            catch (Exception e)
+            {
+                Logger.LogCritical(e, "An unexpected exception was thrown whilst running ToDoListBotWorker. Trying again in 5 minutes");
+                await Task.Delay(TimeSpan.FromMinutes(5), stoppingToken);
+            }
+        }
+        Logger.LogInformation("Ended ToDoListBotWorker exection");
     }
 }
