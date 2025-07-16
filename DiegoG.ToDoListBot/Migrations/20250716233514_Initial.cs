a6c3375d897f7b5e33a6afded4834892dea92e70
Diego García <dagarciam1014@gmail.com>
2025-07-16 18:57:32 -0500

    Added migrations

---
+++ DiegoG.ToDoListBot/Migrations/20250716233514_Initial.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DiegoG.ToDoListBot.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ChatConfigs",
+                columns: table => new
+                {
+                    ChatId = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    UtcOffset = table.Column<TimeSpan>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ChatConfigs", x => x.ChatId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ConversationContexts",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ConversationId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Step = table.Column<long>(type: "INTEGER", nullable: false),
+                    ActiveAction = table.Column<string>(type: "TEXT", nullable: true),
+                    Encoding = table.Column<string>(type: "TEXT", nullable: true),
+                    AssemblyQualifiedContextTypeName = table.Column<string>(type: "TEXT", nullable: true),
+                    JsonData = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ConversationContexts", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ToDoLists",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ChatId = table.Column<long>(type: "INTEGER", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    CronExpression = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ToDoLists", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ToDoLists_ChatConfigs_ChatId",
+                        column: x => x.ChatId,
+                        principalTable: "ChatConfigs",
+                        principalColumn: "ChatId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ToDoTasks",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "INTEGER", nullable: false),
+                    ToDoListId = table.Column<long>(type: "INTEGER", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    IsCompleted = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ToDoTasks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ToDoTasks_ToDoLists_ToDoListId",
+                        column: x => x.ToDoListId,
+                        principalTable: "ToDoLists",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ConversationContexts_ActiveAction",
+                table: "ConversationContexts",
+                column: "ActiveAction");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ConversationContexts_AssemblyQualifiedContextTypeName",
+                table: "ConversationContexts",
+                column: "AssemblyQualifiedContextTypeName");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ConversationContexts_ConversationId",
+                table: "ConversationContexts",
+                column: "ConversationId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ToDoLists_ChatId",
+                table: "ToDoLists",
+                column: "ChatId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ToDoTasks_ToDoListId",
+                table: "ToDoTasks",
+                column: "ToDoListId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ConversationContexts");
+
+            migrationBuilder.DropTable(
+                name: "ToDoTasks");
+
+            migrationBuilder.DropTable(
+                name: "ToDoLists");
+
+            migrationBuilder.DropTable(
+                name: "ChatConfigs");
+        }
+    }
+}
